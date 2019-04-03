44b2c75c2c162d652f3637c3870a793ef4f7c24d
Adam Helinski <adam@dvlopt.io>
2019-04-03 11:19:51 +0200

    Release 1.0.0

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
 (defproject dvlopt/linux.gpio
-            "0.0.0"
+            "1.0.0"
 
   :description  "Use the standard Linux GPIO api from Clojure"
   :url          "https://github.com/dvlopt/linux.gpio.clj"
   :license      {:name "Eclipse Public License"
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[dvlopt/void          "0.0.0"]
-                 [org.clojure/clojure  "1.9.0"]
-                 [io.dvlopt/linux-gpio "1.0.0"]]
+                 [io.dvlopt/linux-gpio "1.0.0"]
+                 [org.clojure/clojure  "1.9.0"]]
   :profiles     {:dev {:source-paths ["dev"
                                       "examples"]
                        :main         user
-                       :dependencies [[org.clojure/test.check "0.10.0-alpha2"]
-                                      [criterium              "0.4.4"]]
-                       :plugins      [[venantius/ultra "0.5.2"]
-                                      [lein-codox      "0.10.3"]]
+                       :dependencies [[criterium              "0.4.4"]
+                                      [org.clojure/test.check "0.10.0-alpha2"]]
+                       :plugins      [[lein-codox      "0.10.3"]
+                                      [venantius/ultra "0.5.2"]]
                        :codox        {:output-path  "doc/auto"
                                       :source-paths ["src"]}
                        :repl-options {:timeout 180000}
